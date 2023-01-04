fe220aa4eb81dc7b734e0b44758248edb3f05b3c
nehol33 <95410575+nehol33@users.noreply.github.com>
2023-01-04 15:26:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
-# format recuperer
-# [[1,[200,400]],[2,[500,600]],[3,[600,900]]]
-# cette liste sera nommé lst_donnees
-# ecrire un programme nommée recup_donnees
-
+#recuperation des donnees sous forme d'une liste de liste
 def recup_donnees():
     lst_donnees = []  #On initialise la liste lst_donnees
     with open('donnees.txt', 'r') as d:   #On ouvre le fichier 
@@ -17,6 +13,38 @@ def recup_donnees():
             col3 = int(col3)
             # maintenant, on peut traiter les données de chaque colonne comme des variables
             lst_donnees.append([col1, [col2, col3]])  #On insère les valeurs dans la liste
-    print(lst_donnees)
+    return(lst_donnees)
 
-recup_donnees()
+# Dessinez le graphique
+def dessiner (G):
+    pos = nx.circular_layout(G)
+    nx.draw_networkx_nodes(G,pos)
+    nx.draw_networkx_edges(G,pos)
+    nx.draw_networkx_labels(G,pos) 
+    plt.show()
+
+def cree_graph_incompatibilite():
+    lst_donnees = recup_donnees()
+    lst_signaux = []
+    # Créez un graphique vide
+    G = nx.Graph()
+    # Ajoutez des noeuds et des arêtes
+    for signal in lst_donnees :
+        G.add_node(signal[0])
+        lst_signaux.append(signal[0])
+    # Ajoute les arretes representant les incompatibilite c'est a dire les superpositions de freq
+    while lst_donnees :
+        signal_selec = lst_donnees[0]
+        lst_donnees.pop(0)
+        for signal_comparé in lst_donnees :
+            if signal_selec[1][0]-10 > signal_comparé[1][1] or signal_selec[1][1]+10 > signal_comparé[1][0]:
+                G.add_edge(signal_selec[0],signal_comparé[0])
+    return G
+
+graph_incompatible = cree_graph_incompatibilite()
+dessiner(graph_incompatible)
+
+
+
+
+
