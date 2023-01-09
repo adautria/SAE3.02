320b309f817d6b76b5f96a39cfe25f69b90c5e78
nehol33 <95410575+nehol33@users.noreply.github.com>
2023-01-09 09:16:44 +0100

    Update cree_donnees.py

--- cree_donnees.py
+++ cree_donnees.py
@@ -1,21 +1,21 @@
 import os
 from random import randint
+def cree_donnees_f(nb_signaux):
+   try:
+      # Envoie le fichier donnees.txt dans la corbeille
+      os.remove('donnees2.txt')
+   except FileNotFoundError:
+      # Le fichier n'existe pas, rien à faire
+      pass
+      
+   fichier = open('donnees.txt', 'w')
 
-try:
-   # Envoie le fichier donnees.txt dans la corbeille
-   os.remove('donnees2.txt')
-except FileNotFoundError:
-   # Le fichier n'existe pas, rien à faire
-   pass
-   
-fichier = open('donnees.txt', 'w')
-
-for i in range (10):
-    freq_min = randint (0,1000)
-    bp = randint (20,500)
-    freq_max = freq_min + bp 
-    ligne = str(i+1) + ' ' + str(freq_min) + ' ' + str(freq_max) + '\n'
-    fichier.write(ligne)
+   for i in range (nb_signaux):
+      freq_min = randint (0,1000)
+      bp = randint (20,500)
+      freq_max = freq_min + bp 
+      ligne = str(i+1) + ' ' + str(freq_min) + ' ' + str(freq_max) + '\n'
+      fichier.write(ligne)
 
 
-fichier.close()
+   fichier.close()
